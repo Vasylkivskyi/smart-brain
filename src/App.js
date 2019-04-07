32302347e4bb88045e0e993d5ccc8a9f63cea10c
Pavlo Vasylkivsliy <pavel.vasylkivskiy@gmail.com>
2019-04-07 20:36:41 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("https://lit-springs-28979.herokuapp.com/imageurl", {
+    fetch('https://lit-springs-28979.herokuapp.com/imageurl', {
         method: "post",
         headers: {
           "Content-Type": "application/json"
@@ -81,7 +81,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response){
-          fetch("https://lit-springs-28979.herokuapp.com/image", {
+          fetch('https://lit-springs-28979.herokuapp.com/image', {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
